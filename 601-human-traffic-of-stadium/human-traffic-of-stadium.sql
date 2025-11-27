6e4acc61d766d2f3f212fe989090a6bc8e8cd032
Ash <189637525+DivaQueen-dev@users.noreply.github.com>
2025-11-27 21:22:31 +0530

    Time: 371 ms (36.13%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,29 +1,12 @@
-SELECT 
-    id
-    ,visit_date
-    ,people
-FROM stadium
-WHERE id IN (
-    SELECT s1.id /*s1 is selected*/
-    FROM stadium s1
-    JOIN stadium s2 ON s1.id=s2.id-1
-    JOIN stadium s3 ON s2.id=s3.id-1
-    WHERE s1.people>=100 AND s2.people>=100 AND s3.people>=100
-
-    UNION
-
-    SELECT s2.id/*s2 is selected*/
-    FROM stadium s1
-    JOIN stadium s2 ON s1.id=s2.id-1
-    JOIN stadium s3 ON s2.id=s3.id-1
-    WHERE s1.people>=100 AND s2.people>=100 AND s3.people>=100
-
-    UNION 
-
-    SELECT s3.id/*s3 is selected*/
-    FROM stadium s1
-    JOIN stadium s2 ON s1.id=s2.id-1
-    JOIN stadium s3 ON s2.id=s3.id-1
-    WHERE s1.people>=100 AND s2.people>=100 AND s3.people>=100
+with q1 as (
+select *, 
+     count(*) over( order by id range between current row and 2 following ) following_cnt,
+     count(*) over( order by id range between 2 preceding and current row ) preceding_cnt,
+     count(*) over( order by id range between 1 preceding and 1 following ) current_cnt
+from stadium
+where people > 99
 )
-GROUP BY visit_date
+select id, visit_date, people
+from q1
+where following_cnt = 3 or preceding_cnt = 3 or current_cnt = 3
+order by visit_date
