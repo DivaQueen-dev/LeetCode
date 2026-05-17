c196346ff56628112c827d4c864f96fbd1d9c187
Asritha <189637525+DivaQueen-dev@users.noreply.github.com>
2026-05-17 22:25:49 +0530

    Time: 1 ms (100.00%) | Memory: 47.4 MB (58.81%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
+        long actualSum = 0, actualSquareSum = 0;
         int n = grid.length;
-        int size = n * n;
-        int[] count = new int[size + 1];
+        long N = (long) n * n;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                count[grid[i][j]]++;
+                int num = grid[i][j];
+                actualSum += num;
+                actualSquareSum += (long) num * num;
             }
         }
 
-        int a = -1, b = -1;
+        long expectedSum = (N * (N + 1)) / 2;
+        long expectedSquareSum = (N * (N + 1) * (2 * N + 1)) / 6;
 
-        for (int num = 1; num <= size; num++) {
-            if (count[num] == 2) {
-                a = num;
-            } else if (count[num] == 0) {
-                b = num;
-            }
-        }
+        long sumDifference = actualSum - expectedSum;
+        long squareSumDifference = actualSquareSum - expectedSquareSum;
 
-        return new int[]{a, b};
+        long sumAb = squareSumDifference / sumDifference;
+
+        int repeated = (int) ((sumAb + sumDifference) / 2);
+        int missing = (int) ((sumAb - sumDifference) / 2);
+
+        return new int[]{repeated, missing};
     }
 }
