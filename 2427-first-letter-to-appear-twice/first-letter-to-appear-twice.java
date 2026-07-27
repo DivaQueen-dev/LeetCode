fb4d1c95964d2f167d4d93cd29e1a3c8d8eccc2e
Asritha <189637525+DivaQueen-dev@users.noreply.github.com>
2026-07-27 21:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (33.98%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        boolean[]seen=new boolean[26];
+        for(char ch: s.toCharArray())
+        {
+            int index=ch-'a';
+        if(seen[index])
+        {
+            return ch;
+        }
+        seen[index]=true;
+
+        }
+return ' ';
+    }
+}
