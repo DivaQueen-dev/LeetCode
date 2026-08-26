3a210fb371fa3bb9b83f1ae06e6b007970e60a86
Asritha <189637525+DivaQueen-dev@users.noreply.github.com>
2026-08-26 11:13:19 +0530

    Time: 53 ms (9.66%) | Memory: 53 MB (80.56%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if (val === other) {
+                return true;
+            }
+            throw new Error("Not Equal");
+        },
+
+        notToBe: function(other) {
+            if (val !== other) {
+                return true;
+            }
+            throw new Error("Equal");
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5);        // true
+ * expect(5).notToBe(5);     // throws Error("Equal")
+ * expect(5).toBe(null);     // throws Error("Not Equal")
+ * expect(5).notToBe(null);  // true
+ */
